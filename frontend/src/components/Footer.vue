3aeebbfe21e83d6f0d11c7aca6554e779dcd834b
AndreiButalov <andrej.butalov@gmx.de>
2026-07-25 22:11:08 +0200

    add footer.vue

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="fixed bottom-0 left-0 w-full h-15 bg-[#FAEBD7] flex items-center justify-center rounded-t-md">
+    <h1>Footer</h1>
+  </footer>
+</template>
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="w-full h-15 bg-[#FAEBD7] flex items-center px-8 absolute rounded-b-md">
-        <h1>hallo</h1>
+        <h1>Navbar</h1>
     </div>    
 </template>
