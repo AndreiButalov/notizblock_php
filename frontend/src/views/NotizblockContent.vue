7070a4bbb74cebcef1cfbbb7b04b71afbf377c8a
AndreiButalov <andrej.butalov@gmx.de>
2026-07-25 21:09:04 +0200

    add class='btn'

--- frontend/src/views/NotizblockContent.vue
+++ frontend/src/views/NotizblockContent.vue
@@ -3,8 +3,8 @@
     <div class="w-100 h-93 rounded-xl bg-[#FAEBD7] p-8 flex flex-col overflow-hidden">
 
       <div class="flex justify-between">
-        <h1>Notizen Hinzufügen</h1>
-        <p>Papierkorb</p>
+        <h1 class="btn">Notizen Hinzufügen</h1>
+        <p class="btn">Papierkorb</p>
       </div>
 
       <div class="flex flex-col flex-1 gap-2 mt-4">
@@ -19,7 +19,7 @@
           class="border-2 border-solid p-2 flex-1 resize-none overflow-y-auto"
         ></textarea>
 
-        <button class="border p-2">
+        <button class="btn">
           Add
         </button>
       </div>
