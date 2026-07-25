78d9d826874c94aa0e2b4126079ef1d7e358047c
AndreiButalov <andrej.butalov@gmx.de>
2026-07-25 22:39:47 +0200

    navbar and footer max-with 1920px, add logo

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <footer class="fixed bottom-0 left-0 w-full h-15 bg-[#FAEBD7] flex items-center justify-center rounded-t-md">
-    <h1>Footer</h1>
+  <footer class="fixed bottom-0 left-0 w-full">
+    <div class="max-w-[1920px] mx-auto h-15 bg-[#FAEBD7] hover:bg-[#e8d5bd] flex items-center justify-center rounded-t-md">
+      <h1>Footer</h1>
+    </div>
   </footer>
 </template>
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="w-full h-15 bg-[#FAEBD7] flex items-center px-8 absolute rounded-b-md">
-        <h1>Navbar</h1>
-    </div>    
+  <div class="absolute top-0 left-0 w-full">
+    <div class="max-w-[1920px] mx-auto h-15 bg-[#FAEBD7] hover:bg-[#e8d5bd] flex items-center px-8 rounded-b-md">
+
+      <div class="flex items-center gap-2">
+        <img src="../assets/img/keep_2020q4_48dp.png" alt="">
+        <h1>Notizblock</h1>
+      </div>
+
+    </div>
+  </div>
 </template>
