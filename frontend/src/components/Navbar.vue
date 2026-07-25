e56d866bf9ea44ca11f994764afdb729c76c80c7
AndreiButalov <andrej.butalov@gmx.de>
2026-07-25 21:46:46 +0200

    add Navbar.vue

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-[#FAEBD7] w-100 h-20">
+    <div class="w-full h-15 bg-[#FAEBD7] flex items-center px-8 absolute">
         <h1>hallo</h1>
     </div>    
 </template>
