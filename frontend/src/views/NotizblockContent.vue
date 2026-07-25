a0976b1182c9ace68035ee7924c52f808ee68fd0
AndreiButalov <andrej.butalov@gmx.de>
2026-07-25 20:54:23 +0200

    css-> update

--- frontend/src/views/NotizblockContent.vue
+++ frontend/src/views/NotizblockContent.vue
@@ -1,16 +1,34 @@
 <template>
-  <div class="notizblock-content">
-    <h1>Notizblock</h1>
-    <p>Hier kommt dein Inhalt hin.</p>
+  <div class="flex min-h-screen items-center justify-center">
+    <div class="w-100 h-93 rounded-xl bg-[#FAEBD7] p-8 flex flex-col overflow-hidden">
+
+      <div class="flex justify-between">
+        <h1>Notizen Hinzufügen</h1>
+        <p>Papierkorb</p>
+      </div>
+
+      <div class="flex flex-col flex-1 gap-2 mt-4">
+        <input 
+          type="text" 
+          placeholder="Title" 
+          class="border-2 border-solid p-2"
+        >
+
+        <textarea 
+          placeholder="Notizen"
+          class="border-2 border-solid p-2 flex-1 resize-none overflow-y-auto"
+        ></textarea>
+
+        <button class="border p-2">
+          Add
+        </button>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script setup>
 </script>
 
-<style scoped>
-.notizblock-content {
-  padding: 2rem;
-  font-family: Arial, sans-serif;
-}
-</style>
+
