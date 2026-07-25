9ba1de6944dcb1b9dd0688b7a9a60c07a5859cc6
AndreiButalov <andrej.butalov@gmx.de>
2026-07-25 22:47:35 +0200

    add impressum and Datenschutz

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,7 +1,10 @@
 <template>
   <footer class="fixed bottom-0 left-0 w-full">
     <div class="max-w-[1920px] mx-auto h-15 bg-[#FAEBD7] hover:bg-[#e8d5bd] flex items-center justify-center rounded-t-md">
-      <h1>Footer</h1>
+      <div class="flex gap-8">
+        <h1 class="link">Impressum</h1>
+        <h1 class="link">Datenschutz</h1>
+      </div>
     </div>
   </footer>
 </template>
