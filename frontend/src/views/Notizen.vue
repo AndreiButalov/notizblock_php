fc17e5aea1a999a96b122fffa9e07e167d817283
AndreiButalov <andrej.butalov@gmx.de>
2026-07-26 10:26:10 +0200

    main.css -> update

--- frontend/src/views/Notizen.vue
+++ frontend/src/views/Notizen.vue
@@ -3,6 +3,12 @@
     <div class="w-100 h-93 rounded-xl bg-[#FAEBD7] p-8 flex flex-col overflow-hidden">
       <h1>Hallo</h1>
       <router-link class="link" to="/addNotizen">Add Notizen</router-link>
+      <div>
+
+      </div>
     </div>
   </div>
 </template>
+<script setup>
+    
+</script>
